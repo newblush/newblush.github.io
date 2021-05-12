c8ce89d3a1892b2736aed5d4fd956c509138a20f
MLN <171250532@smail.nju.edu.cn>
2021-05-12 14:27:47 +0800

    Site updated: 2021-05-12 14:27:46

---
+++ APlayer/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+        {
+            name: "Bones",
+            artist: 'Low Roar / Jofridur Akadottir',
+            url: 'http://music.163.com/song/media/outer/url?id=472045267.mp3',
+            cover: 'http://p1.music.126.net/NFRfRmK8-xHkNgUYiL-FRQ==/109951163563295425.jpg?param=130y130',
+        }
+    ]
+});
